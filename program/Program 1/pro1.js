c9f21225b1a5662ff020013eccf5e6c556acd412
bhavy2408 <bhavy0033@gmail.com>
2026-07-08 18:32:47 +0530

    08-07-2026

---
+++ program/Program 1/pro1.js
@@ -0,0 +1,9 @@
+//  The Four Outputs: Write a TypeScript program that displays a welcome message using
+//  all four output methods covered in class: console.log(), document.write(), alert(),
+//  and innerHTML. 
+
+console.log("Welcome to the program!");
+document.write("Welcome to the program!");
+alert("Welcome to the program!");
+document.getElementById("welcomeMessage").innerHTML = "Welcome to the program!";
+
