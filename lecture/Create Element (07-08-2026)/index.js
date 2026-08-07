b3e31a38a42d007be999888130c225a6cd69c4ed
bhavy2408 <bhavy0033@gmail.com>
2026-08-07 18:45:36 +0530

    07-08-2026

---
+++ lecture/Create Element (07-08-2026)/index.js
@@ -0,0 +1,57 @@
+// Create Element 
+
+// Syntax 
+// document.createElement("tagname")
+
+// {
+//     const div = document.createElement("div");
+
+//     // Added Content 
+//     div.innerText = "Hello World"
+//     console.log(div);
+    
+//     // Insert Elment 5 Method
+
+//     // 1. appendChild()
+//     document.body.appendChild(div)
+
+//     // 2. append()
+//     document.body.append(div, "javascript")
+
+//     // 3. prepend()
+//     document.body.prepend(div)
+
+//     // 4. After()
+//     {
+//         let str = "javascript"
+//         document.body.after(str)
+//     }
+//     // 5. Before()
+//     {
+//         let str = "javascript"
+//         document.body.before(str)
+//     }
+
+// }
+
+{
+    // // Remove Elment
+    let parent = document.getElementById("parent");
+    // parent.remove()
+
+    // let list = document.getElementById("list")
+
+    // const child = list.children[1]
+    // list.removeChild(child)
+
+    // console.log(child);
+
+    // Traversing
+
+    console.log(list.firs);
+
+
+    
+
+
+}
