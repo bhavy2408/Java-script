4461feeb3fda8c01258a86bdd04322543f41e4df
bhavy2408 <bhavy0033@gmail.com>
2026-08-31 16:58:26 +0530

    31-08-2026

---
+++ lecture/Import And Export (31-08-2026)/js/Helper.js
@@ -0,0 +1,4 @@
+import add from './Home.js'
+import {multi} from './Home.js'
+console.log(add(5,3))
+console.log(multi(5,3));
---
+++ lecture/Import And Export (31-08-2026)/js/Home.js
@@ -0,0 +1,10 @@
+let add = (a,b) => {
+    return a + b
+}
+
+export default add;
+
+
+export let multi = (a,b) => {
+    return a * b;
+}
